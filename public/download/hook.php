758d44880fa49d78559bb3b8650274ed608feeff
Steve Robbins <steven.j.robbins@gmail.com>
2015-05-27 14:12:49 -0700

    added phpci

--- public/download/hook.php
+++ public/download/hook.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Mage Scan
+ *
+ * PHP version 5
+ *
+ * @category  MageScan
+ * @package   MageScan
+ * @author    Steve Robbins <steven.j.robbins@gmail.com>
+ * @copyright 2015 Steve Robbins
+ * @license   http://creativecommons.org/licenses/by/4.0/ CC BY 4.0
+ * @link      https://github.com/steverobbins/magescan
+ */
 
 ini_set('display_errors', 1);
 
